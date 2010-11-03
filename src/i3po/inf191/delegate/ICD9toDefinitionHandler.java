04bdd82481a8238ee4e926bf045b5b16cf0de51a
Jordaniel <jwolk@uci.edu>
2010-11-03 00:44:23 -0700

    renamed Handlers and Delegates

--- src/i3po/inf191/delegate/ICD9toDefinitionHandler.java
+++ src/i3po/inf191/delegate/ICD9toDefinitionHandler.java
@@ -1,7 +1,5 @@
 package i3po.inf191.delegate;
 
-import java.util.Iterator;
-
 import org.apache.axiom.om.OMElement;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -11,11 +9,6 @@ public class ICD9toDefinitionHandler {
 
 
 	public String handleRequest(OMElement request) {
-		for(Iterator<OMElement> i = request.getAllAttributes(); i.hasNext(); ) {
-			OMElement next = i.next();
-			log.info("[INFO] " + next.toString());
-			System.out.println(next.toString());
-		}
-		return "ok. Done.";
+		return "hi there";
 	}
 }
