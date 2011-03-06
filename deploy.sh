a8a071a8917e9febe89b4a53f7dd528a3aeaf711
inyofam <inyofam@inyofam-donkeykong.(none)>
2011-03-05 20:36:16 -0800

    working on README

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #xjc -d ./src ./src/DEF_QRY.xsd
 #if [ -n "${TOMCAT_HOME:+nonnull}" ]
-if [ -z "$TOMCAT_HOME" ] || [ ! -d $TOMCAT_HOME ]; then
+if [ -z "$TOMCAT_HOME" ] || [ ! -d $TOMCAT_HOME ] || [ ! -w $TOMCAT_HOME ]; then
 	echo ""
-	echo "You must set TOMCAT_HOME (in .bashrc, /etc/environment, or in the current shell) in order to deploy i3po"
+	echo "You must set TOMCAT_HOME (in .bashrc, /etc/environment, or in the current shell) in order to deploy i3po. Be sure to omit the trailing '/'."
 	echo ""
 	echo "If your TOMCAT_HOME is not world-writeable, try executing as root with 'sudo su'"
 	echo ""
