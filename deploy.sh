f67a07ba6008493463d776da09ddddf8a3a6a084
inyofam <inyofam@inyofam-donkeykong.(none)>
2011-03-05 19:55:26 -0800

    deploy.sh updated

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #xjc -d ./src ./src/DEF_QRY.xsd
 #if [ -n "${TOMCAT_HOME:+nonnull}" ]
-if [ -z "$TOMCAT_HOME" ]; then
-	echo "\nYou must set TOMCAT_HOME (in .bashrc or in the current shell) in order to deploy i3po\n"	
+if [ -z "$TOMCAT_HOME" ] || [ ! -d $TOMCAT_HOME ]; then
+	echo ""
+	echo "You must set TOMCAT_HOME (in .bashrc, /etc/environment, or in the current shell) in order to deploy i3po"
+	echo ""
+	echo "If your TOMCAT_HOME is not world-writeable, try executing as root with 'sudo su'"
+	echo ""
 else
 	I3PO=.
 	xjc -p i3po.inf191.xsd -d $I3PO/src $I3PO/src/
@@ -11,8 +15,8 @@ else
 	mv DefinitionService.aar $TOMCAT_HOME/webapps/axis2/WEB-INF/services/
 	cd ../../../
 	cp -R ./* $TOMCAT_HOME/webapps/axis2/WEB-INF/classes/
-	$TOMCAT_HOME/bin/shutdown.sh
+	$TOMCAT_HOME/bin/shutdown.sh > /dev/null
 	echo "Wait one moment while Tomcat restarts\n"
-	ping -c 2 www.google.com
+	ping -c 2 www.google.com > /dev/null
 	$TOMCAT_HOME/bin/startup.sh
 fi
