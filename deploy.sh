b18cb76284a870acd169e291305fbfc80660e7e8
inyofam <inyofam@inyofam-donkeykong.(none)>
2011-03-05 18:36:15 -0800

    updated deploy.sh and removed old stuff

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,18 @@
 #xjc -d ./src ./src/DEF_QRY.xsd
-xjc -p i3po.inf191.xsd -d ./src ./src/
-cp ./lib/*.jar /opt/apache-tomcat-7.0.8/webapps/axis2/WEB-INF/lib
-cd ./bin/i3po/inf191/axis2/
-jar -cvf DefinitionService.aar ./*
-mv DefinitionService.aar /opt/apache-tomcat-7.0.4/webapps/axis2/WEB-INF/services/
-cd ../../../
-cp -R ./* /opt/apache-tomcat-7.0.4/webapps/axis2/WEB-INF/classes/
-/opt/apache-tomcat-7.0.4/bin/shutdown.sh
-echo "Wait one moment...pinging google to kill time\n"
-ping -c 2 www.google.com
-/opt/apache-tomcat-7.0.4/bin/startup.sh
+#if [ -n "${TOMCAT_HOME:+nonnull}" ]
+if [ -z "$TOMCAT_HOME" ]; then
+	echo "\nYou must set TOMCAT_HOME (in .bashrc or in the current shell) in order to deploy i3po\n"	
+else
+	I3PO=.
+	xjc -p i3po.inf191.xsd -d $I3PO/src $I3PO/src/
+	cp $I3PO/lib/*.jar $TOMCAT_HOME/webapps/axis2/WEB-INF/lib
+	cd $I3PO/bin/i3po/inf191/axis2/
+	jar -cvf DefinitionService.aar ./*
+	mv DefinitionService.aar $TOMCAT_HOME/webapps/axis2/WEB-INF/services/
+	cd ../../../
+	cp -R ./* $TOMCAT_HOME/webapps/axis2/WEB-INF/classes/
+	$TOMCAT_HOME/bin/shutdown.sh
+	echo "Wait one moment while Tomcat restarts\n"
+	ping -c 2 www.google.com
+	$TOMCAT_HOME/bin/startup.sh
+fi
