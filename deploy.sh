a4ac10e32089504eed1370cf688cfe5bdd84035e
Jordaniel <jwolk@uci.edu>
2011-03-16 05:12:40 -0700

    tweaked deploy.sh; still not perfect

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,16 @@ if [ -z "$TOMCAT_HOME" ] || [ ! -d $TOMCAT_HOME ] || [ ! -w $TOMCAT_HOME ]; then
 	echo ""
 	echo "You must set TOMCAT_HOME (in .bashrc, /etc/environment, or in the current shell) in order to deploy i3po. Be sure to omit the trailing '/'."
 	echo ""
-	echo "If your TOMCAT_HOME is not world-writeable, try executing as root with 'sudo su'"
+	echo "If your TOMCAT_HOME is not world-writeable, try executing 'sudo env TOMCAT_HOME=\$TOMCAT_HOME ./deploy.sh'."
 	echo ""
 else
-	I3PO=.
+	I3PO=`pwd`
 	xjc -p i3po.inf191.xsd -d $I3PO/src $I3PO/src/
 	cp $I3PO/lib/*.jar $TOMCAT_HOME/webapps/axis2/WEB-INF/lib
 	cd $I3PO/bin/i3po/inf191/axis2/
 	jar -cvf DefinitionService.aar ./*
 	mv DefinitionService.aar $TOMCAT_HOME/webapps/axis2/WEB-INF/services/
-	cd ../../../
+	cd $I3PO/bin/
 	cp -R ./* $TOMCAT_HOME/webapps/axis2/WEB-INF/classes/
 	$TOMCAT_HOME/bin/shutdown.sh > /dev/null
 	echo "Wait one moment while Tomcat restarts\n"
